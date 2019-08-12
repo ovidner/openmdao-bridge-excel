f5e013211f0a8f7459b11f747bd79881adf567fb
Olle Vidner <olle@vidner.se>
2019-08-12 16:00:13 +0200

    Add tests for macro execution

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,7 +3,7 @@ import hypothesis.strategies as st
 import numpy as np
 import openmdao.api as om
 import pytest
-from hypothesis import assume, given, settings
+from hypothesis import given, settings
 
 from openmdao_bridge_excel import ExcelComponent
 from openmdao_utils.external_tools import VarMap
@@ -22,6 +22,37 @@ def test_continuous_finite_scalar(value):
             file_path="tests/data/passthrough.xlsx",
             inputs=[VarMap("in", "a")],
             outputs=[VarMap("out", "b")],
+        ),
+    )
+    model.connect("indeps.x", "passthrough.in")
+
+    prob.setup()
+    prob.run_model()
+    prob.cleanup()
+
+    # Using a normal == comparison will not consider NaNs as equal.
+    assert np.allclose(prob["indeps.x"], value, atol=0.0, rtol=0.0, equal_nan=True)
+    assert np.allclose(
+        prob["passthrough.out"], prob["indeps.x"], atol=0.0, rtol=0.0, equal_nan=True
+    )
+
+
+@settings(deadline=2000)
+@given(st.floats(allow_nan=False, allow_infinity=False))
+def test_continuous_finite_scalar_macros(value):
+    prob = om.Problem()
+    model = prob.model
+
+    model.add_subsystem("indeps", om.IndepVarComp("x", val=value))
+    model.add_subsystem(
+        "passthrough",
+        ExcelComponent(
+            file_path="tests/data/passthrough_macros.xlsm",
+            inputs=[VarMap("in", "a")],
+            outputs=[VarMap("out", "b")],
+            pre_macros=["NameA", "NameB"],
+            main_macros=["CopyAToB"],
+            post_macros=["EnsureBEqualsA"],
         ),
     )
     model.connect("indeps.x", "passthrough.in")
